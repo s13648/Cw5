24088bc700890a535bf741b2529fa58fc1ac6bde
Misha Kurch <s13648@pjwstk.edu.pl>
2021-01-06 16:43:35 +0100

    -Dodanie try catch do controllerow -Uzycie constant w ststus codach

--- Cw5/Controllers/EnrollmentsController.cs
+++ Cw5/Controllers/EnrollmentsController.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Cw5.Dto;
 using Cw5.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cw5.Controllers
@@ -23,28 +25,42 @@ namespace Cw5.Controllers
         [HttpPost]
         public async Task<IActionResult> EnrollStudent(EnrollStudent model)
         {
-            var study = await studyDbService.GetByName(model.Studies);
-            if (study == null)
-                return BadRequest($"Study with name: {model.Studies} not found");
+            try
+            {
+                var study = await studyDbService.GetByName(model.Studies);
+                if (study == null)
+                    return BadRequest($"Study with name: {model.Studies} not found");
 
-            if (await studentDbService.Exists(model.IndexNumber))
-                return BadRequest($"Student with index: {model.Studies} already exists");
+                if (await studentDbService.Exists(model.IndexNumber))
+                    return BadRequest($"Student with index: {model.Studies} already exists");
 
-            var enrollment = await enrollmentDbService.EnrollStudent(model,study);
+                var enrollment = await enrollmentDbService.EnrollStudent(model,study);
 
-            return StatusCode(201, enrollment);
+                return StatusCode(StatusCodes.Status201Created, enrollment);
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost("promotions")]
         public async Task<IActionResult> Promotions(Promotions promotions)
         {
-            if (!await enrollmentDbService.Exists(promotions.Studies, promotions.Semester))
-                return BadRequest("Promotion not found");
+            try
+            {
+                if (!await enrollmentDbService.Exists(promotions.Studies, promotions.Semester))
+                    return BadRequest("Promotion not found");
 
 
-            await enrollmentDbService.Promotions(promotions);
+                await enrollmentDbService.Promotions(promotions);
 
-            return StatusCode(201, await enrollmentDbService.GetBy(promotions.Studies, promotions.Semester + 1));
+                return StatusCode(StatusCodes.Status201Created, await enrollmentDbService.GetBy(promotions.Studies, promotions.Semester + 1));
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
